1eaa2c1f8ff9c996b3e9de4a01037a3f3407dc25
tolik518 <function@returnnull.de>
2024-05-01 00:50:05 +0200

    added header parsing and response

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::io::{Read, Write};
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::net::Incoming;
 use itertools::Itertools;
+
+struct Request {
+    method: String,
+    path: String,
+    version: String,
+    headers: Vec<(String, String)>
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,10 +19,22 @@ fn main() {
     for stream in listener.incoming() {
         match &stream {
             Ok(_stream) => {
-                let mut tcp_stream = stream.unwrap();
-                let client_ip = &tcp_stream.peer_addr().unwrap();
-                println!("Client ip connected: {client_ip}");
-                tcp_stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap()
+                let mut tcp_stream = &stream.unwrap();
+
+                // create a buffer to read the stream into
+                let request = get_client_request_data(&mut tcp_stream);
+                let request = parse_request(request.trim());
+
+                if request.path == "/" {
+                    tcp_stream
+                        .write_all(response_builder(200, "OK"))
+                        .unwrap();
+                } else {
+                    tcp_stream
+                        .write_all(response_builder(404, "Not Found"))
+                        .unwrap();
+                }
+
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -26,4 +43,50 @@ fn main() {
     }
 
     println!("==== Program finished ====");
+}
+
+fn response_builder(code: u16, code_status: &str) -> &[u8] {
+    let response = format!{"HTTP/1.1 {code} {code_status}\r\n\r\n"};
+    return response.as_bytes();
+}
+
+fn get_client_request_data(tcp_stream: &mut &TcpStream) -> String {
+    let mut client_request = String::new();
+    let mut buffer: Vec<char> = Vec::new();
+    for byte in tcp_stream.bytes() {
+        //const ESCAPE_SEQUENCE: [char; 4] = ['\r', '\n', '\r', '\n'];
+        if buffer.ends_with(&['\r', '\n', '\r']) {
+            client_request = buffer.iter().join("");
+            break;
+        }
+
+        buffer.push(byte.unwrap() as char);
+    }
+
+    return client_request;
+}
+
+fn parse_request(client_request: &str) -> Request {
+    let mut request = Request {
+        method: String::new(),
+        path: String::new(),
+        version: String::new(),
+        headers: Vec::new()
+    };
+
+    let mut lines = client_request.lines();
+    let first_line = lines.next().unwrap();
+    let mut first_line_parts = first_line.split_whitespace();
+    request.method = first_line_parts.next().unwrap().to_string();
+    request.path = first_line_parts.next().unwrap().to_string();
+    request.version = first_line_parts.next().unwrap().to_string();
+
+    for line in lines {
+        //split line on :
+        let header_parts: Vec<&str> = line.split(": ").collect();
+        //push to headers
+        request.headers.push((header_parts[0].to_string(), header_parts[1].to_string()));
+    }
+
+    return request;
 }
