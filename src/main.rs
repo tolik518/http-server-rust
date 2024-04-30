c76a809fd08f8e8a4e78f19647cc1228c08ad925
tolik518 <function@returnnull.de>
2024-04-30 23:28:46 +0200

    added tcp stream write

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::net::Incoming;
+use itertools::Itertools;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    println!("==== Program running ====");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
+
     for stream in listener.incoming() {
-        match stream {
+        match &stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                let mut tcp_stream = stream.unwrap();
+                let client_ip = &tcp_stream.peer_addr().unwrap();
+                println!("Client ip connected: {client_ip}");
+                tcp_stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    println!("==== Program finished ====");
 }
